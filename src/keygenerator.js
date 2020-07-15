3cd8003f4a4930f3f526ec72e2aac76233db9ed8
Denis Andreenko <andreenkodn@gmail.com>
2020-07-15 12:52:05 +0300

    refactoring&docs: add docs, code refactoring

--- src/keygenerator.js
+++ src/keygenerator.js
@@ -1,6 +1,9 @@
 const EC = require('elliptic').ec;
+
+// You can use any elliptic curve you want
 const ec = new EC('secp256k1');
 
+// Generate a new key pair and convert them to hex-strings
 const key = ec.genKeyPair();
 const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
-const {Blockchain, Transaction} = require('./blockchain');
+const { Blockchain, Transaction } = require('./blockchain');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-const myKey = ec.keyFromPrivate('ec04c674dabbe6df28d99516b70b36c3099965eb5ab3764003466389bc3cb12f');
+// Your private key goes here
+const myKey = ec.keyFromPrivate('3d6f54430830d388052865b95c10b4aeb1bbe33c01334cf2cfa8b520062a0ce3');
+
+// Public key (Wallet address)
 const myWalletAddress = myKey.getPublic('hex');
 
-let customCoin = new Blockchain();
+// Create new Blockchain instance
+const customCoin = new Blockchain();
 
-const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+// Create a transaction and sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 10);
 tx1.signTransaction(myKey);
 customCoin.addTransaction(tx1);
 
-console.log('\n Starting the miner...');
+// Mine block
+customCoin.minePendingTransactions(myWalletAddress);
+
+const tx2 = new Transaction(myWalletAddress, 'address1', 30);
+tx2.signTransaction(myKey);
+customCoin.addTransaction(tx2);
+
+// Mine block
 customCoin.minePendingTransactions(myWalletAddress);
 
 console.log('\nMy balance is ', customCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('Is chain valid?', customCoin.isChainValid());
+console.log('Is chain valid?', customCoin.isChainValid() ? 'Yes' : 'No');
